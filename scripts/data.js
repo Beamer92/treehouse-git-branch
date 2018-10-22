22739aab86204b13be2630b85473cb719493ddd2
Brendan Woodell <brendan.woodell@gmail.com>
2018-10-22 10:38:15 -0700

    added Mont-Blac or whatever treehouse

--- scripts/data.js
+++ scripts/data.js
@@ -16,6 +16,13 @@ const treehouses = [
     img: 'https://media.architecturaldigest.com/photos/55e76412cd709ad62e8e6854/master/w_640,c_limit/dam-images-architecture-2013-07-tree-houses-treehouse-architecture-01-la-piantata-suite-bleue-italy.jpg',
     description: 'A centuries-old majestic oak is home to Suite Bleue, one of two tree houses at La Piantata Bed & Breakfast in Italy’s northern Lazio region. The simple red-cedar building was constructed in France by the Provence-based specialty firm La Cabane Perchée and transported to a sprawling field of lavender on the resort’s estate.',
     contributor: 'Wes'
+  },
+  {
+    title: 'Monbazillac',
+    img:'https://d36tnp772eyphs.cloudfront.net/blogs/1/2014/10/Monbazillac-a.jpg',
+    description: 'In Aquitaine, France, the Chateaux dans les Arbres features the Monbazillac Treehouse, looks like Hogwarts if Hogwarts was small and a treehouse',
+    contributor: 'Brendan'
+
   }
 ]
 
